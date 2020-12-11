d36b50a2911d589579f9739d5bb140149d015fc9
yzh0308 <61719854+yzh0308@users.noreply.github.com>
2020-12-11 12:00:53 +0800

    A107260058-楊宗翰-SHU-R-Midterm-sourc

---
+++ A107260058-楊宗翰-SHU-R-Midterm-source.R
@@ -0,0 +1,15 @@
+# 2020/12/11(五), 109學年第一學期 資料科學應用 R期中考
+#
+# 學號: A107260058      姓名: 楊宗翰
+#
+# 本檔案為各題之程式碼檔，無執行結果
+
+
+# ex2(a)
+install.packages("tidyverse",repos = "http://cran.us.r-project.org")
+library(tidyverse)
+install.packages("readxl",repos = "http://cran.us.r-project.org")
+library(readxl)
+
+
+
